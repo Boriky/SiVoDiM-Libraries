3ca9a99a203d934ca3f649730d9cf695e9306343
Stark-Labs <starklabs.swe@gmail.com>
2016-06-16 12:23:49 +0200

    export play

--- app/src/main/java/starklabs/sivodim/Drama/Model/Utilities/SpeechSound.java
+++ app/src/main/java/starklabs/sivodim/Drama/Model/Utilities/SpeechSound.java
@@ -1,11 +1,13 @@
 package starklabs.sivodim.Drama.Model.Utilities;
 
+import android.widget.MediaController;
+
 import java.io.File;
 
 /**
  * Created by Francesco Bizzaro on 25/05/2016.
  */
-public class SpeechSound extends Sound {
+public class SpeechSound extends Sound implements MediaController.MediaPlayerControl {
     private static final long maxSize=15728640L;
 
     public SpeechSound(String path){
@@ -15,5 +17,55 @@ public class SpeechSound extends Sound {
     @Override
     protected long maxSize() {
         return maxSize;
+    }
+
+    @Override
+    public void start() {
+        play();
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int pos) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }
