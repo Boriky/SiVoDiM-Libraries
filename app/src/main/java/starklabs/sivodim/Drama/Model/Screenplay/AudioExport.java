14957d1a2b5bff7f272af2c4e06e3c25b84e52ef
Stark-Labs <starklabs.swe@gmail.com>
2016-07-01 18:15:56 +0200

    Fix audio export algorithm

--- app/src/main/java/starklabs/sivodim/Drama/Model/Screenplay/AudioExport.java
+++ app/src/main/java/starklabs/sivodim/Drama/Model/Screenplay/AudioExport.java
@@ -9,7 +9,9 @@
         import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 
         import java.io.File;
+        import java.util.ArrayList;
         import java.util.Iterator;
+        import java.util.List;
         import java.util.ListIterator;
 
         import starklabs.sivodim.Drama.Model.Chapter.Chapter;
@@ -22,74 +24,120 @@
  */
 public class AudioExport extends ExportAlgorithm {
 
-    private void concatenateSpeeches(final Context context){
-        File dir=new File(screenplay.getPath(context));
-        if(!dir.exists()){
-            dir.mkdir();
-        }
+     private void concatenateSpeeches(final Context context, int i, final List<File> chapterExportes, final Iterator<Chapter> chapterIterator){
+         if(chapterIterator.hasNext()){// join the speeches of the chapter
+             String name=screenplay.getTitle().replace(" ","_");
+             final File destination=new File(context.getFilesDir(),"ch"+i+name+".wav");
+             chapterExportes.add(destination);
+             i++;
+             final int finalI=i;
+             AudioConcatenator audioConcatenator=new AudioConcatenator(context,destination);
+             audioConcatenator.setDestination(destination);
+             final Chapter chapter=chapterIterator.next();
+             ListIterator<Speech>speechListIterator=chapter.getSpeechIterator();
+             while (speechListIterator.hasNext()){
+                 Speech speech=speechListIterator.next();
+                 while (speech.getAudioStatus()==false){
+                     System.out.println("Battuta si sta scaricando");
+                     try {
+                         Thread.sleep(10);
+                     } catch (InterruptedException e) {
+                         e.printStackTrace();
+                     }
+                 }
+                 File speechFile=new File(speech.getAudioPath());
+                 audioConcatenator.addFile(speechFile);
+             }
+             try {
+                 audioConcatenator.exec(new FFmpegExecuteResponseHandler() {
+                     @Override
+                     public void onSuccess(String message) {
 
-        String name=screenplay.getTitle().replace(" ","_");
-        File destination=new File(context.getFilesDir(),"concatenation"+name+".wav");
-        AudioConcatenator audioConcatenator=new AudioConcatenator(context,destination);
-        audioConcatenator.setDestination(destination);
-        Iterator<Chapter>chapterIterator= screenplay.getChapterIterator();
-        while (chapterIterator.hasNext()){
-            Chapter chapter=chapterIterator.next();
-            ListIterator<Speech>speechListIterator=chapter.getSpeechIterator();
-            while (speechListIterator.hasNext()){
-                Speech speech=speechListIterator.next();
-                while (speech.getAudioStatus()==false){
-                    System.out.println("Battuta si sta scaricando");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                File speechFile=new File(speech.getAudioPath());
-                audioConcatenator.addFile(speechFile);
-            }
-        }
-        try {
-            audioConcatenator.exec(new FFmpegExecuteResponseHandler() {
-                @Override
-                public void onSuccess(String message) {
+                     }
 
-                }
+                     @Override
+                     public void onProgress(String message) {
 
-                @Override
-                public void onProgress(String message) {
-                    System.out.println(message);
-                }
+                     }
 
-                @Override
-                public void onFailure(String message) {
+                     @Override
+                     public void onFailure(String message) {
 
-                }
+                     }
 
-                @Override
-                public void onStart() {
+                     @Override
+                     public void onStart() {
 
-                }
+                     }
 
-                @Override
-                public void onFinish() {
-                    finalizeExport(context);
-                }
-            });
-        } catch (FFmpegCommandAlreadyRunningException e) {
-            e.printStackTrace();
-        }
-    }
+                     @Override
+                     public void onFinish() {
+                        if(chapter.getSoundtrack()!=null && chapter.getSoundtrack().getAudio()!=null
+                                && chapter.getSoundtrack().getAudio().exists())
+                            addSoundtrack(context,finalI,chapterExportes,chapterIterator,chapter,destination.getName());
+                         else
+                            concatenateSpeeches(context,finalI,chapterExportes,chapterIterator);
+                     }
+                 });
+             } catch (FFmpegCommandAlreadyRunningException e) {
+                 e.printStackTrace();
+             }
 
-   /*  private File concatenateSpeeches(Context context,File chapterExportes, Soundtrack soundtrack){
+         }else{// join all chapters
+             File dir=new File(screenplay.getPath(context));
+             if(!dir.exists()){
+                 dir.mkdir();
+             }
+             String name=screenplay.getTitle().replace(" ","_");
+             File destination=new File(context.getFilesDir(),"concatenation"+name+".wav");
+             AudioConcatenator audioConcatenator=new AudioConcatenator(context,destination);
+             audioConcatenator.setDestination(destination);
+             for (File file: chapterExportes){
+                 if(file.exists())
+                     audioConcatenator.addFile(file);
+             }
+             try {
+                 audioConcatenator.exec(new FFmpegExecuteResponseHandler() {
+                     @Override
+                     public void onSuccess(String message) {
 
-     }*/
+                     }
 
-    private File addSoundtrack(Context context,File chapterExportes, Soundtrack soundtrack){
-        String name=chapterExportes.getName();
-        File destination=new File(context.getFilesDir(),"merged_"+name);
-        AudioMixer audioMixer=new AudioMixer(context,chapterExportes,soundtrack.getAudio(),destination);
+                     @Override
+                     public void onProgress(String message) {
+                         System.out.println(message);
+                     }
+
+                     @Override
+                     public void onFailure(String message) {
+
+                     }
+
+                     @Override
+                     public void onStart() {
+
+                     }
+
+                     @Override
+                     public void onFinish() {
+                         finalizeExport(context);
+                     }
+                 });
+             } catch (FFmpegCommandAlreadyRunningException e) {
+                 e.printStackTrace();
+             }
+         }
+     }
+
+    private File addSoundtrack(final Context context,final int i, final List<File> chapterExportes,
+                               final Iterator<Chapter> chapterIterator, Chapter chapter,String dest){
+        File destination=new File(context.getFilesDir(),"merged_"+dest);
+        File primary=chapterExportes.remove(chapterExportes.size()-1);
+        AudioMixer audioMixer=new AudioMixer(context,
+                primary,
+                chapter.getSoundtrack().getAudio(),
+                destination);
+        chapterExportes.add(destination);
         try {
             audioMixer.exec(new FFmpegExecuteResponseHandler() {
                 @Override
@@ -114,7 +162,7 @@ public class AudioExport extends ExportAlgorithm {
 
                 @Override
                 public void onFinish() {
-
+                    concatenateSpeeches(context,i,chapterExportes,chapterIterator);
                 }
             });
         } catch (FFmpegCommandAlreadyRunningException e) {
@@ -165,171 +213,7 @@ public class AudioExport extends ExportAlgorithm {
 
     @Override
     public void export(Context context) {
-        concatenateSpeeches(context);
-    }
-}
-
-
-
-
-
-
-/*package starklabs.sivodim.Drama.Model.Screenplay;
-
-import android.content.Context;
-import android.widget.Toast;
-
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.ListIterator;
-import java.util.Vector;
-
-import starklabs.sivodim.Drama.Model.Chapter.Chapter;
-import starklabs.sivodim.Drama.Model.Chapter.Speech;
-import starklabs.sivodim.Drama.Model.Utilities.Soundtrack;*/
-
-/**
- * Created by Francesco Bizzaro on 25/05/2016.
- */
-/*public class AudioExport extends ExportAlgorithm {
-
-    private void concatenateSpeeches(Context context){
-        File dir=new File(screenplay.getPath(context));
-        if(!dir.exists()){
-            dir.mkdir();
-        }
-        File dirPatials=new File(dir,"Partials");
-        if(!dirPatials.exists()){
-            dirPatials.mkdir();
-        }
-        String name=screenplay.getTitle().replace(" ","_");
-        File destination=new File(context.getFilesDir(),"concatenation"+name+".wav");
-        Vector<File>chapterAudios=new Vector<>();
         Iterator<Chapter>chapterIterator= screenplay.getChapterIterator();
-        int i=0;
-        while (chapterIterator.hasNext()){
-            Chapter chapter=chapterIterator.next();
-            System.out.println(chapter.getTitle().toUpperCase());
-            File chapterConcat=new File(context.getFilesDir(),"concat"+i+name+".wav");
-            if(!chapterConcat.exists()){
-                try {
-                    chapterConcat.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            chapterConcat=speechChapter(context,chapter,chapterConcat);
-            /*AudioConcatenator chapterConcatenator=new AudioConcatenator(context,chapterConcat);
-            chapterConcatenator.setDestination(chapterConcat);
-            ListIterator<Speech>speechListIterator=chapter.getSpeechIterator();
-            while (speechListIterator.hasNext()){
-                Speech speech=speechListIterator.next();
-                while (speech.getAudioStatus()==false){
-                    System.out.println("Battuta"+i+"si sta scaricando");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                File speechFile=new File(speech.getAudioPath());
-                chapterConcatenator.addFile(speechFile);
-            }
-            try {
-                chapterConcatenator.exec();
-            } catch (FFmpegCommandAlreadyRunningException e) {
-                e.printStackTrace();
-            }
-            Toast.makeText(context,"Esportato "+chapter.getTitle(),Toast.LENGTH_SHORT).show();
-            if(chapter.getSoundtrack()!=null &&
-                    chapter.getSoundtrack().getAudio()!=null &&
-                    chapter.getSoundtrack().getAudio().exists())
-                chapterAudios.add(addSoundtrack(context,chapterConcat,chapter.getSoundtrack()));
-            else
-                chapterAudios.add(chapterConcat);
-            i++;
-        }
-
-        AudioConcatenator audioConcatenator=new AudioConcatenator(context,destination);
-        audioConcatenator.setDestination(destination);
-        Iterator<File>fileIterator=chapterAudios.iterator();
-        while (fileIterator.hasNext()){
-            File fileExp=fileIterator.next();
-            audioConcatenator.addFile(fileExp);
-        }
-        try {
-            audioConcatenator.exec();
-        } catch (FFmpegCommandAlreadyRunningException e) {
-            e.printStackTrace();
-        }
-        // delete garbage from physical memory
-        /*Iterator<File>fileIterator2=chapterAudios.iterator();
-        while (fileIterator2.hasNext()){
-            File fileExp=fileIterator2.next();
-            if(fileExp.exists())
-                fileExp.delete();
-        }*/
-    /*}
-
-    private File speechChapter(Context context,Chapter chapter,File destination){
-        AudioConcatenator chapterConcatenator=new AudioConcatenator(context,destination);
-        chapterConcatenator.setDestination(destination);
-        ListIterator<Speech>speechListIterator=chapter.getSpeechIterator();
-        int i=0;
-        while (speechListIterator.hasNext()){
-            Speech speech=speechListIterator.next();
-            // check if audio is still downloading
-            while (speech.getAudioStatus()==false){
-                System.out.println("Battuta"+i+"si sta scaricando");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            File speechFile=new File(speech.getAudioPath());
-            chapterConcatenator.addFile(speechFile);
-            i++;
-        }
-        try {
-            chapterConcatenator.exec();
-        } catch (FFmpegCommandAlreadyRunningException e) {
-            e.printStackTrace();
-        }
-        return destination;
-    }
-
-    private File addSoundtrack(Context context,File chapterExportes, Soundtrack soundtrack){
-        String name=chapterExportes.getName();
-        File destination=new File(context.getFilesDir(),"merged_"+name);
-        AudioMixer audioMixer=new AudioMixer(context,chapterExportes,soundtrack.getAudio(),destination);
-        try {
-            audioMixer.exec();
-        } catch (FFmpegCommandAlreadyRunningException e) {
-            e.printStackTrace();
-        }
-        return destination;
-    }
-
-    private void finalizeExport(Context context){
-        String name=screenplay.getTitle().replace(" ","_");
-        File file=new File(context.getFilesDir(),"concatenation"+name+".wav");
-        File destination=new File(context.getFilesDir(),name+".mp3");
-        Mp3Converter mp3Converter=new Mp3Converter(context,file,destination);
-        try {
-            mp3Converter.exec();
-        } catch (FFmpegCommandAlreadyRunningException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void export(Context context) {
-        concatenateSpeeches(context);
-        finalizeExport(context);
+        concatenateSpeeches(context,0,new ArrayList<File>(),chapterIterator);
     }
 }
-*/
