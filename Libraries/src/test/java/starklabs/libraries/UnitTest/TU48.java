8c18485870645e671cd723cec518a0f5088d2f2a
Stark-Labs <starklabs.swe@gmail.com>
2016-07-07 14:29:15 +0200

    implement TU40 and TU48

--- Libraries/src/test/java/starklabs/libraries/UnitTest/TU48.java
+++ Libraries/src/test/java/starklabs/libraries/UnitTest/TU48.java
@@ -4,8 +4,18 @@ package starklabs.libraries.UnitTest;
  * Created by GINO on 27/06/2016.
  */
 
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import starklabs.libraries.Model.Mivoq.AudioRequest;
 
@@ -14,10 +24,45 @@ import static org.junit.Assert.assertEquals;
 /**
  * Test TU47 that tests the correct creation of an AudioRequest object
  */
-public class TU48 {
+public class TU48 extends InstrumentationTestCase{
+    private boolean correct=false;
+
     @Test
     public void testAudioRequestCreation(){
-        AudioRequest audioRequest=Mockito.mock(AudioRequest.class);
-        assertEquals(true,!audioRequest.equals(null));
+        final Context context=getInstrumentation().getContext();
+        Map<String, String> Params = new HashMap<String, String>();
+
+        //Insert parameters for the Mivoq Service
+        Params.put("input[type]","TEXT");
+        Params.put("input[content]","testo di prova");
+        Params.put("input[locale]","it");
+        Params.put("output[type]","AUDIO");
+        Params.put("output[format]","WAVE_FILE");
+        Params.put("voice[gender]","male");
+        Params.put("voice[name]","");
+        Params.put("voice[selection_algorythm","ssml");
+        Params.put("utterance[effects]","");
+
+        AudioRequest audioRequest=new AudioRequest(Request.Method.POST, "http://fic2fatts.tts.mivoq.it/say", Params,
+                new Response.Listener<byte[]>()
+                {
+                    @Override
+                    public void onResponse(byte[] myResponse) {
+                        //Saving of the response in the byte array
+                        synchronized(this) {
+                            System.out.println("Consegna avvenuta con successo");
+                            assertTrue(true);
+                            this.notifyAll();
+                        }
+                    }}
+                , new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                System.out.println("c'e' stato un errore nell'invio della richiesta");
+                System.out.println(error.getMessage());
+                assertTrue(false);
+            }
+        } );
     }
 }
--- app/src/test/java/starklabs/sivodim/UnitTest/TU40.java
+++ app/src/test/java/starklabs/sivodim/UnitTest/TU40.java
@@ -4,23 +4,67 @@ import android.app.Activity;
 import android.content.Context;
 import android.test.InstrumentationTestCase;
 
+import com.github.hiteshsondhi88.libffmpeg.FFmpegExecuteResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import starklabs.sivodim.Drama.Model.Screenplay.AudioConcatenator;
 import starklabs.sivodim.Drama.Model.Screenplay.AudioMixer;
 
 /**
- * Created by io on 07/07/2016.
+ * Created by Francesco Bizzaro on 07/07/2016.
  */
 public class TU40 extends InstrumentationTestCase {
     @Test
     public void testAudioConcatenator(){
         Context context=getInstrumentation().getContext();
-        File destination=new File(context.getFilesDir(),"");
+        final File destination=new File(context.getFilesDir(),"concat.wav");
         AudioConcatenator audioConcatenator=new AudioConcatenator(context,destination);
+        assertNotNull(audioConcatenator);
+        List<File> files=new ArrayList<>();
+        files.add(new File(context.getFilesDir(),"parz0.wav"));
+        files.add(new File(context.getFilesDir(),"parz1.wav"));
+        files.add(new File(context.getFilesDir(),"parz2.wav"));
+        files.add(new File(context.getFilesDir(),"parz3.wav"));
+        files.add(new File(context.getFilesDir(),"parz4.wav"));
+        for (File file:files){
+            audioConcatenator.addFile(file);
+        }
+        try {
+            audioConcatenator.exec(new FFmpegExecuteResponseHandler() {
+                @Override
+                public void onSuccess(String message) {
 
+                }
+
+                @Override
+                public void onProgress(String message) {
+
+                }
+
+                @Override
+                public void onFailure(String message) {
+
+                }
+
+                @Override
+                public void onStart() {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    assertTrue(destination.exists());
+                }
+            });
+        } catch (FFmpegCommandAlreadyRunningException e) {
+            e.printStackTrace();
+        }
     }
 }
