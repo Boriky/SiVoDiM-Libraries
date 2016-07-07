47c3a3804bfd2bb5c862370569fe05d1100ef044
onlinebooks <onlinelibro@gmail.com>
2016-07-07 14:30:19 +0200

    Implemented TU41 and TU42 (working)

--- app/src/test/java/starklabs/sivodim/UnitTest/TU41.java
+++ app/src/test/java/starklabs/sivodim/UnitTest/TU41.java
@@ -7,6 +7,10 @@ package starklabs.sivodim.UnitTest;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Context;
+import android.test.InstrumentationTestCase;
+
+import com.github.hiteshsondhi88.libffmpeg.FFmpegExecuteResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -21,24 +25,54 @@ import static org.junit.Assert.assertEquals;
 /**
  * Test TU41 that tests AudioMixer functionality
  */
-public class TU41 extends Instrumentation{
+public class TU41 extends InstrumentationTestCase{
     @Test
     public void testAudioMixer(){
-        Context mContext = new Activity();
-        Context context=Mockito.mock(Context.class);
+        Context context=getInstrumentation().getContext();
         File primary=new File("C:Desktop/prova.wav");
         File secondary=new File("C:Desktop/prova1.wav");
-        File destination=new File("C:Desktop/end.wav");
+        final File destination=new File("C:Desktop/end.wav");
 
-        AudioMixer audioMixer=new AudioMixer(mContext.getApplicationContext(),primary,secondary,destination);
+        AudioMixer audioMixer=new AudioMixer(context,primary,secondary,destination);
         audioMixer.setDestination(new File("C:Desktop/test/end.wav"));
         audioMixer.setPrimaryFile(new File("C:Desktop/test/prova.wav"));
         audioMixer.setSecondaryFile(new File("C:Descktop/test/prova1.wav") );
         File testD=new File("C:Desktop/test/end.wav");
         File testP=new File("C:Desktop/test/prova.wav");
         File testS=new File("C:Desktop/test/prova1.wav");
+        try {
+            audioMixer.exec(new FFmpegExecuteResponseHandler() {
+                @Override
+                public void onSuccess(String message) {
 
-        assertEquals(true,!audioMixer.equals(null));
+                }
+
+                @Override
+                public void onProgress(String message) {
+
+                }
+
+                @Override
+                public void onFailure(String message) {
+
+                }
+
+                @Override
+                public void onStart() {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    assertEquals(true,destination.exists());
+
+                }
+            });
+        } catch (FFmpegCommandAlreadyRunningException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 }
