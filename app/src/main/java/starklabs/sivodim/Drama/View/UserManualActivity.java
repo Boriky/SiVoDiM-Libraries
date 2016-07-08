93b0218b65e41c4356b62a397a0e8df82a3c4f45
Stark-Labs <starklabs.swe@gmail.com>
2016-07-08 14:53:32 +0200

    update user manual

--- app/src/main/java/starklabs/sivodim/Drama/View/UserManualActivity.java
+++ app/src/main/java/starklabs/sivodim/Drama/View/UserManualActivity.java
@@ -23,20 +23,19 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         userManual=(TextView)findViewById(R.id.userManual);
-        userManual.setText(Html.fromHtml("\n" +
-                "<h1>Manuale utente</h1>\n" +
+        userManual.setText(Html.fromHtml("<h1>Manuale utente</h1>\n" +
                 "<h2>1 Introduzione</h2>\n" +
                 "<h3>1.1 Scopo del documento</h3>\n" +
                 "<p>Questo documento rappresenta il manuale d’uso indirizzato all’utente finale delle applicazioni\n" +
                 "Speeches e SiVoDiM, sviluppate per la realizzazione del progetto universitario SiVoDiM:\n" +
-                "Sintesi Vocale per Dispositivi Mobili. Il documento è disponibile direttamente dal drawerG\n" +
+                "Sintesi Vocale per Dispositivi Mobili. Il documento è disponibile direttamente dal drawer\n" +
                 "delle app e ha lo scopo di descrivere le modalità d’utilizzo di tutte le funzionalità offerte,\n" +
                 "suddivise in opportune sezioni.</p>\n" +
                 "\n" +
                 "<h3>1.2 Scopo del prodotto</h3>\n" +
                 "<p>Il prodotto si pone l’obiettivo di dimostrare efficacemente le potenzialità del motore di sintesi\n" +
-                "vocale FA-TTSG, realizzato dall’azienda MIVOQ s.r.l. e messo a disposizione del team di\n" +
-                "sviluppo Stark Labs. Il team rende disponibili due applicazioni per sistemi AndroidG:</p>\n" +
+                "vocale FA-TTS, realizzato dall’azienda MIVOQ s.r.l. e messo a disposizione del team di\n" +
+                "sviluppo Stark Labs. Il team rende disponibili due applicazioni per sistemi Android:</p>\n" +
                 "<ul>\n" +
                 "<li>Speeches: permette la creazione e il salvataggio di racconti e sceneggiati, che possono\n" +
                 "essere esportati in formato audio (o video) attraverso l’utilizzo del motore FA-TTS;</li>\n" +
@@ -54,37 +53,37 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<h2>2 Guida all’installazione</h2>\n" +
                 "<h3>2.1 Requisiti di sistema</h3>\n" +
                 "<p>Per poter utilizzare SiVoDiM è necessario disporre di un dispositivo smartphone equipaggiato\n" +
-                "con sistema operativo AndroidG 4.0 o superiore.</p>\n" +
+                "con sistema operativo Android 4.0 o superiore.</p>\n" +
                 "<h3>2.2 Installazione</h3>\n" +
                 "<p>L’applicazione non è ancora disponibile nella piattaforma Google Play. Pertanto è necessario\n" +
                 "installarla manualmente seguendo la procedura che segue:</p>\n" +
                 "<ol>\n" +
-                "<li>Scaricare l’apkG dalla pagina ufficiale del team di sviluppo;</li>\n" +
-                "<li>Salvare l’apk nella memoria SDG del dispositivo;</li>\n" +
+                "<li>Scaricare l’apk dalla pagina ufficiale del team di sviluppo;</li>\n" +
+                "<li>Salvare l’apk nella memoria SD del dispositivo;</li>\n" +
                 "<li>Aprire le Impostazioni del dispositivo ed entrare nel menu Sicurezza;</li>\n" +
                 "<li>Scorrere il menu e abilitare la voce Origini sconosciute;</li>\n" +
                 "<li>Selezionare l’apk dalla memoria del dispositivo:\n" +
                 "<ul><li>Se l’applicazione è stata scaricata direttamente dal dispositivo, l’apk si può\n" +
                 "eseguire dalla cartella Download (opzione di default);</li>\n" +
                 "<li>Se l’applicazione è stata scaricata da PC, è necessario trasferirla tramite connessione\n" +
-                "USBG al dispositivo e disporre di un gestore file per navigare nella cartella\n" +
+                "USB al dispositivo e disporre di un gestore file per navigare nella cartella\n" +
                 "in cui è stata copiata.</li></ul></li>\n" +
-                "<li>Seguire la procedura guidata di AndroidG per completare il processo di installazione.</li></ol>\n" +
+                "<li>Seguire la procedura guidata di Android per completare il processo di installazione.</li></ol>\n" +
                 "\n" +
                 "<h2>3 Guida alle funzionalità di Speeches</h2>\n" +
                 "<h3>3.1 Navigare fra le schermate principali</h3>\n" +
                 "<p>Avviata l’applicazione è possibile accedere a uno sceneggiato (al primo avvio è disponibile\n" +
                 "uno sceneggiato di prova). Nell’angolo in alto a sinistra è presente il pulsante di accesso al\n" +
-                "drawerG. Dal drawer è possibile accedere all’applicazione di configurazione SiVoDiM,\n" +
+                "drawer. Dal drawer è possibile accedere all’applicazione di configurazione SiVoDiM,\n" +
                 "al manuale utente o alle info dell’app.\n" +
                 "All’interno di uno sceneggiato si possono visualizzare i capitoli interni. Nella barra degli\n" +
-                "strumenti è possibile esportare rapidamente lo sceneggiato in formato MP3G e condividerlo\n" +
+                "strumenti è possibile esportare rapidamente lo sceneggiato in formato MP3 e condividerlo\n" +
                 "su altre applicazioni attraverso la pressione delle rispettive icone. Nell’angolo in alto a destra\n" +
                 "è presente il pulsante di accesso al menu delle opzioni, che rende disponibili le seguenti\n" +
                 "voci:</p>\n" +
                 "<ul>\n" +
                 "<li>Salva progetto: salva una copia locale dello sceneggiato aperto;</li>\n" +
-                "<li>Esporta in video: esporta lo sceneggiato in locale nel formato MP4G (video);</li>\n" +
+                "<li>Esporta in video: esporta lo sceneggiato in locale nel formato MP4 (video);</li>\n" +
                 "<li>Condividi video: condividi lo sceneggiato esportato in video su social network o\n" +
                 "applicazioni di messaggistica;</li>\n" +
                 "<li>Crea personaggio: apre la schermata dedicata alla creazione di un nuovo personaggio;</li>\n" +
@@ -95,7 +94,8 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "la pressione della rispettiva icona. Nell’angolo in alto a destra è presente il pulsante\n" +
                 "di accesso al menu delle opzioni ( ), che rende disponibili le seguenti voci:</p>\n" +
                 "<ul>\n" +
-                "<li>Modifica capitolo: abilita la modifica di titolo, soundtrackG e immagine di sfondo;</li>\n" +
+                "<li>Modifica capitolo: abilita la modifica di titolo, soundtrack\n" +
+                " e immagine di sfondo;</li>\n" +
                 "<li>Lista personaggi: accesso alla lista completa di tutti i personaggi appartenenti allo\n" +
                 "sceneggiato aperto.</li></ul>\n" +
                 "<h3>3.2 Creare un nuovo sceneggiato</h3\n" +
@@ -110,7 +110,7 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "personaggi.</li>\n" +
                 "<li>Confermare la creazione premendo l’apposita icona posta nell’angolo in alto a destra\n" +
                 "dello schermo.</li></ol>\n" +
-                "<p>A questo punto lo sceneggiato è disponibile nella homeG.\n" +
+                "<p>A questo punto lo sceneggiato è disponibile nella home.\n" +
                 "Premendo il pulsante che visualizza il nome dello sceneggiato, è possibile navigare all’interno\n" +
                 "di esso e iniziare a popolarlo con nuovi capitoli e rispettive battute.</p>\n" +
                 "<h3>3.3 Creare un nuovo capitolo<h3>\n" +
@@ -122,12 +122,12 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<li>Assegnare un nome al capitolo;</li>\n" +
                 "<li>Premere il pulsante CARICA SOUNDTRACK e caricare una traccia audio che funge\n" +
                 "da sottofondo sonoro al capitolo (opzionale):\n" +
-                "(a) Navigare fra i file presenti nella memoria SDG e selezionare la traccia audio\n" +
-                "desiderata (formati supportati: MP3G e WAVG).</li>\n" +
+                "(a) Navigare fra i file presenti nella memoria SD e selezionare la traccia audio\n" +
+                "desiderata (formati supportati: MP3 e WAV).</li>\n" +
                 "<li>Premere il pulsante CARICA SFONDO e caricare un’immagine che funge da sfondo al\n" +
                 "capitolo (opzionale):\n" +
                 "(a) Navigare fra i file presenti nella galleria del dispositivo e selezionare l’immagine\n" +
-                "desiderata (formati supportati: JPGG e PNGG).</li>\n" +
+                "desiderata (formati supportati: JPG e PNG).</li>\n" +
                 "<li>Confermare la creazione premendo l’apposita icona posta nell’angolo in alto a destra\n" +
                 "dello schermo.</li></ol>\n" +
                 "<p>A questo punto il capitolo è disponibile nella schermata interna allo sceneggiato.\n" +
@@ -155,7 +155,7 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<li>Assegnare un nome al personaggio;</li>\n" +
                 "<li>Assegnare una voce al personaggio;</li>\n" +
                 "<li>Testare la voce premendo sul pulsante TEST VOCE (opzionale);</li>\n" +
-                "<li>Caricare un avatarG dalla memoria SDG del dispositivo:\n" +
+                "<li>Caricare un avatar dalla memoria SD del dispositivo:\n" +
                 "(a) Selezionare una fra le applicazioni offerte per la visualizzazione delle immagini;\n" +
                 "(b) Navigare nella galleria per selezionare l’immagine desiderata: l’immagine verrà\n" +
                 "rifiutata se la dimensione supera 1MB.</li>\n" +
@@ -163,7 +163,7 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<p>Ad operazione conclusa verrà aperta la schermata contenente una lista di tutti i personaggi\n" +
                 "creati all’interno dello sceneggiato.</p>\n" +
                 "<h3>3.6 Eliminare uno sceneggiato</h3>\n" +
-                "<p>Dalla homeG dell’applicazione effettuare una pressione prolungata sullo sceneggiato che si\n" +
+                "<p>Dalla home dell’applicazione effettuare una pressione prolungata sullo sceneggiato che si\n" +
                 "vuole eliminare. Cliccare quindi sul pulsante di eliminazione posto nell’angolo in basso a\n" +
                 "destra dello schermo.</p>\n" +
                 "<h3>3.7 Eliminare un capitolo</h3>\n" +
@@ -200,13 +200,13 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<h3>3.13 Esportare lo sceneggiato</h3>\n" +
                 "<p>Dalla schermata interna a uno sceneggiato accedere al menu a tendina posto nell’angolo in\n" +
                 "alto a destra dello schermo:</p><ol>\n" +
-                "<li>Selezione di una delle due opzioni di esportazione: audio (MP3G) o video (MP4G).</li>\n" +
+                "<li>Selezione di una delle due opzioni di esportazione: audio (MP3) o video (MP4).</li>\n" +
                 "<li>Avvio dell’esportazione dello sceneggiato.</li></ol>\n" +
                 "<p>Il prodotto esportato può essere visionato attraverso:</p><ul>\n" +
                 "<li>Un’applicazione per la riproduzione dell’audio nel caso di esportazione in formato\n" +
-                "MP3G;</li>\n" +
+                "MP3;</li>\n" +
                 "<li>Un’applicazione di tipo galleria per la visualizzazione delle immagini nel caso di esportazione\n" +
-                "in formato MP4G.</li></ul>\n" +
+                "in formato MP4.</li></ul>\n" +
                 "<h3>3.14 Condividere lo sceneggiato</h3>\n" +
                 "<p>Dalla schermata interna a uno sceneggiato cliccare sull’icona di condivisione posta in alto a\n" +
                 "destra dello schermo nella barra degli strumenti:</p><ol>\n" +
@@ -218,7 +218,7 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<h3>4.1 Navigare fra le schermate principali</h3>\n" +
                 "<p>Avviata l’applicazione è possibile accedere alla schermata per la creazione di una nuova voce\n" +
                 "e alla schermata contenente la lista di tutti le voci disponibili. Nell’angolo in alto a sinistra è\n" +
-                "presente il pulsante di accesso al drawerG. Dal drawer è possibile accedere al manuale\n" +
+                "presente il pulsante di accesso al drawer. Dal drawer è possibile accedere al manuale\n" +
                 "utente o alle info dell’app.</p>\n" +
                 "\n" +
                 "<h3>4.2 Creare una nuova voce</h3>\n" +
@@ -227,7 +227,7 @@ public class UserManualActivity extends AppCompatActivity implements UserManualI
                 "<li>Inserire un nome per identificare univocamente la voce;</li>\n" +
                 "<li>Selezionare il sesso della voce (Maschio, Femmina, Neutro, Sconosciuto);</li>\n" +
                 "<li>Selezionare la lingua della voce (Italiano, Inglese, Tedesco, Francese);</li>\n" +
-                "<li>Modificare i parametri designati negli sliderG per ottenere una voce personalizzata:<ul>\n" +
+                "<li>Modificare i parametri designati negli slider per ottenere una voce personalizzata:<ul>\n" +
                 "<li>Altezza: definisce la frequenza media della voce;</li>\n" +
                 "<li>Profondità: altera il tono della voce per donarle più o meno corposità;</li>\n" +
                 "<li>Energia: altera il tono della voce per donarle più o meno vivacità;</li>\n" +
